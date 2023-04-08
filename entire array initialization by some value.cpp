cde526767cb94cff772b34f22d3f23ed5d242c87
Shiva Chunbuk <127691335+shiva017@users.noreply.github.com>
2023-04-08 16:38:08 +0530

    Update entire array initialization by some value.cpp

--- entire array initialization by some value.cpp
+++ entire array initialization by some value.cpp
@@ -1,7 +1,7 @@
 //entire array ko kisi bhi value se intialize krne ka tarika
 #include<bits/stdc++.h>
 using namespace std;
-int maxc[100000]={0};//but this is not work on other
+int maxc[100000]={0};//but this is not work on other becaue se sirf pahla value hota hia baki to automatic 0 manta hai wo 1 krenge to bhi 1 0th index wla hoga but baki to 0 hi hoga
 int arr[1000000];
 int main(){
     fill_n(arr,1000000,5);// fill_n command
